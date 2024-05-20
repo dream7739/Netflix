1f7358502cfa54e16d95085556acec076e1d31b6
홍정민 <dream7739@naver.com>
2024-05-20 22:34:53 +0900

    HomeViewController 함수로 UI 변경

--- Netflix/HomeViewController.swift
+++ Netflix/HomeViewController.swift
@@ -38,83 +38,42 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = .black
-
-        navigationItem.title = "홍정민님"
-        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor : UIColor.white,
-            .font : UIFont.boldSystemFont(ofSize: 20)]
+        
+        designNavigationBar(titleText: "홍정민님")
         
         //메인 포스터 이미지
-        posterImageView.image = UIImage(named: "아바타")
-        posterImageView.contentMode = .scaleAspectFill
-        posterImageView.layer.cornerRadius = 15
-        
-        //포스터 하단 추천 키워드 레이블
-        recommandTagLabel.text = "응원하고픈 · 흥미진진 · 사극 · 전투 · 한국작품"
-        recommandTagLabel.textAlignment = .center
-        recommandTagLabel.textColor = .white
-        recommandTagLabel.font = .systemFont(ofSize: 15)
-        
-        //지금 뜨는 컨텐츠 레이블
-        hotContentLabel.text = "지금 뜨는 컨텐츠"
-        hotContentLabel.textColor = .white
-        hotContentLabel.font = .boldSystemFont(ofSize: 20)
-        hotContentLabel.textAlignment = .left
-        
-        //재생 버튼
-        playButton.setTitle("재생", for: .normal)
-        playButton.setImage(.play, for: .normal)
-        playButton.backgroundColor = .white
-        playButton.tintColor = .black
-        playButton.layer.cornerRadius = 5
-        
-        //내가 찜한 리스트 버튼
-        favoriteListButton.setTitle("내가 찜한 리스트", for: .normal)
-        favoriteListButton.setImage(UIImage(systemName: "plus"), for: .normal)
-        favoriteListButton.backgroundColor = .darkGray
-        favoriteListButton.tintColor = .white
-        favoriteListButton.layer.cornerRadius = 5
+        designImageView(posterImageView, imageName: "아바타", cornerValue: 15)
         
         //지금 뜨는 콘텐츠 이미지뷰1
-        hotContentImageView1.image = UIImage(named: "알라딘")
-        hotContentImageView1.contentMode = .scaleAspectFill
-        hotContentImageView1.layer.cornerRadius = 10
+        designImageView(hotContentImageView1, imageName: "알라딘", cornerValue: 10)
         
         //지금 뜨는 콘텐츠 이미지뷰2
-        hotContentImageView2.image = UIImage(named: "오펜하이머")
-        hotContentImageView2.contentMode = .scaleAspectFill
-        hotContentImageView2.layer.cornerRadius = 10
+        designImageView(hotContentImageView2, imageName: "오펜하이머", cornerValue: 10)
         
         //지금 뜨는 콘텐츠 이미지뷰3
-        hotContentImageView3.image = UIImage(named: "스즈메의문단속")
-        hotContentImageView3.contentMode = .scaleAspectFill
-        hotContentImageView3.layer.cornerRadius = 10
+        designImageView(hotContentImageView3, imageName: "스즈메의문단속", cornerValue: 10)
+        
+        //포스터 하단 추천 키워드 레이블
+        designLabel(recommandTagLabel, labelText: "응원하고픈 · 흥미진진 · 사극 · 전투 · 한국작품", textColor: .white, alignmentAttribute:  .center, fontAttribute: .systemFont(ofSize: 15))
+        
+        //지금 뜨는 컨텐츠 레이블
+        designLabel(hotContentLabel, labelText: "지금 뜨는 컨텐츠", textColor: .white, alignmentAttribute: .left, fontAttribute: .boldSystemFont(ofSize: 20))
+        
+        //재생 버튼
+        designButton(playButton, titleText: "재생", buttonImage: "play", buttonColor: .white, tintColor: .black, cornerValue: 5)
+
+        //내가 찜한 리스트 버튼
+        designButton(favoriteListButton, titleText: "내가 찜한 리스트", buttonImage: "plus", buttonColor: .darkGray, tintColor: .white, cornerValue: 5)
         
         //새로운 에피소드 레이블
-        newEpisodeLabel.text = "새로운에피소드"
-        newEpisodeLabel.textColor = .white
-        newEpisodeLabel.backgroundColor = .red
-        newEpisodeLabel.layer.cornerRadius = 5
-        newEpisodeLabel.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        newEpisodeLabel.clipsToBounds = true
-        newEpisodeLabel.textAlignment = .center
-        newEpisodeLabel.font = .boldSystemFont(ofSize: 12)
+        designLabel(newEpisodeLabel, labelText: "새로운에피소드", textColor: .white, alignmentAttribute: .center, fontAttribute: .boldSystemFont(ofSize: 12))
         
         //지금 시청하기 레이블
-        directPlayLabel.text = "지금 시청하기"
-        directPlayLabel.textColor = .red
-        directPlayLabel.backgroundColor = .white
-        directPlayLabel.textAlignment = .center
-        directPlayLabel.font = .boldSystemFont(ofSize: 12)
-
+        designLabel(directPlayLabel, labelText: "지금 시청하기", textColor: .red, alignmentAttribute: .center, fontAttribute: .boldSystemFont(ofSize: 12))
+        
         //새로운 시리즈 레이블
-        newSeriesLabel.text = "새로운시리즈"
-        newSeriesLabel.textColor = .white
-        newSeriesLabel.backgroundColor = .red
-        newSeriesLabel.layer.cornerRadius = 5
-        newSeriesLabel.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        newSeriesLabel.clipsToBounds = true
-        newSeriesLabel.textAlignment = .center
-        newSeriesLabel.font = .boldSystemFont(ofSize: 14)
+        designLabel(newSeriesLabel, labelText: "새로운시리즈", textColor: .white, alignmentAttribute: .center, fontAttribute: .boldSystemFont(ofSize: 14))
+        
     }
     
     //재생 버튼 클릭 시 4개의 포스터 이미지 변경
@@ -146,10 +105,43 @@ class HomeViewController: UIViewController {
         newEpisodeLabel.isHidden = shuffledBadge[2]
         directPlayLabel.isHidden = shuffledBadge[3]
         newSeriesLabel.isHidden = shuffledBadge[4]
-
+        
     }
     
+    fileprivate func designNavigationBar(titleText: String){
+        navigationItem.title = titleText
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor : UIColor.white,
+                                                                   .font : UIFont.boldSystemFont(ofSize: 20)]
+    }
     
+    fileprivate func designImageView(_ sender: UIImageView, imageName: String, cornerValue: CGFloat){
+        sender.image = UIImage(named: imageName)
+        sender.contentMode = .scaleAspectFill
+        sender.layer.cornerRadius = cornerValue
+    }
     
-    
+    fileprivate func designLabel(_ sender: UILabel, labelText: String, textColor: UIColor, alignmentAttribute:  NSTextAlignment, fontAttribute: UIFont){
+        sender.text = labelText
+        sender.textAlignment = alignmentAttribute
+        sender.textColor = textColor
+        sender.font = fontAttribute
+        
+        if sender == newSeriesLabel || sender == newEpisodeLabel {
+            sender.backgroundColor = .red
+            sender.layer.cornerRadius = 5
+            sender.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+            sender.clipsToBounds = true
+        }else if sender == directPlayLabel {
+            sender.backgroundColor = .white
+            
+        }
+    }
+
+    fileprivate func designButton(_ sender: UIButton, titleText: String, buttonImage: String, buttonColor: UIColor, tintColor: UIColor, cornerValue: CGFloat){
+        sender.setTitle(titleText, for: .normal)
+        sender.setImage(UIImage(named: buttonImage), for: .normal)
+        sender.backgroundColor = buttonColor
+        sender.tintColor = tintColor
+        sender.layer.cornerRadius = 5
+    }
 }
