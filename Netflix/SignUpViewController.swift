767adb4774a4a60fd9fd7c4a1162bd04a8c6e5ab
홍정민 <dream7739@naver.com>
2024-05-20 23:18:34 +0900

    SignUpViewController UI 함수로 구성

--- Netflix/SignUpViewController.swift
+++ Netflix/SignUpViewController.swift
@@ -6,9 +6,9 @@
 //
 
 import UIKit
- 
-class SignUpViewController: UIViewController {
 
+class SignUpViewController: UIViewController {
+    
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var nicknameTextField: UITextField!
@@ -32,64 +32,72 @@ class SignUpViewController: UIViewController {
         }
     }
     
+    @IBAction func keyboardDismiss(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //이메일
-        emailTextField.backgroundColor = .darkGray
-        emailTextField.tintColor = .white
-        emailTextField.textColor = .white
-        emailTextField.attributedPlaceholder = NSAttributedString(string: "이메일 주소 또는 전화번호", attributes: [.foregroundColor: UIColor.white])
-        emailTextField.textAlignment = .center
+        designTextField(emailTextField, placeholderText: "이메일 주소 또는 전화번호")
         
         //비밀번호
-        passwordTextField.backgroundColor = .darkGray
-        passwordTextField.tintColor = .white
-        passwordTextField.textColor = .white
-        passwordTextField.attributedPlaceholder = NSAttributedString(string: "비밀번호", attributes: [.foregroundColor: UIColor.white])
-        passwordTextField.textAlignment = .center
-        passwordTextField.keyboardType = .numberPad
-        passwordTextField.isSecureTextEntry = true
+        designTextField(passwordTextField, placeholderText: "비밀번호")
         
         //닉네임
-        nicknameTextField.backgroundColor = .darkGray
-        nicknameTextField.tintColor = .white
-        nicknameTextField.textColor = .white
-        nicknameTextField.attributedPlaceholder = NSAttributedString(string: "닉네임", attributes: [.foregroundColor: UIColor.white])
-        nicknameTextField.textAlignment = .center
+        designTextField(nicknameTextField, placeholderText: "닉네임")
         
         //위치
-        locationTextField.backgroundColor = .darkGray
-        locationTextField.tintColor = .white
-        locationTextField.textColor = .white
-        locationTextField.attributedPlaceholder = NSAttributedString(string: "위치", attributes: [.foregroundColor: UIColor.white])
-        locationTextField.textAlignment = .center
+        designTextField(locationTextField, placeholderText: "위치")
         
         //추천 코드 입력
-        recommandTextField.backgroundColor = .darkGray
-        recommandTextField.tintColor = .white
-        recommandTextField.textColor = .white
-        recommandTextField.attributedPlaceholder = NSAttributedString(string: "추천 코드 입력", attributes: [.foregroundColor: UIColor.white])
-        recommandTextField.textAlignment = .center
-        recommandTextField.keyboardType = .numberPad
+        designTextField(recommandTextField, placeholderText: "추천 코드 입력")
         
         //회원가입 버튼
-        signUpButton.setTitle("회원가입", for: .normal)
-        signUpButton.backgroundColor = .white
-        signUpButton.layer.cornerRadius = 5
-        signUpButton.tintColor = .black
+        designButton(signUpButton, titleText: "회원가입", buttonColor: .white, tintColor: .black, cornerValue: 5)
         
         //추가 정보 입력 스위치
-        additionalSwitch.onTintColor = .systemPink //켰을 때 컬러
-        additionalSwitch.thumbTintColor = .lightGray //동그라미 컬러
+        designSwitch(additionalSwitch, onColor: .systemPink, thumbColor: .lightGray)
         
         //유효성검사 레이블
-        effectivenessLabel.text = "유효성 검사"
-        effectivenessLabel.textColor = .red
-        effectivenessLabel.font = .systemFont(ofSize: 13)
+        designLabel(effectivenessLabel, labelText: "유효성 검사", textColor: .red, fontAttribute: .systemFont(ofSize: 13))
     }
     
-
-   
-
+    
+    fileprivate func designTextField(_ sender : UITextField, placeholderText: String){
+        sender.backgroundColor = .darkGray
+        sender.tintColor = .white
+        sender.textColor = .white
+        sender.attributedPlaceholder = NSAttributedString(string: placeholderText, attributes: [.foregroundColor: UIColor.white])
+        sender.textAlignment = .center
+        
+        if sender == passwordTextField{
+            sender.keyboardType = .numberPad
+            sender.isSecureTextEntry = true
+        }else if sender == recommandTextField{
+            sender.keyboardType = .numberPad
+            
+        }
+        
+    }
+    
+    fileprivate func designButton(_ sender: UIButton, titleText: String, buttonColor: UIColor, tintColor: UIColor, cornerValue: CGFloat){
+        sender.setTitle(titleText, for: .normal)
+        sender.backgroundColor = buttonColor
+        sender.tintColor = tintColor
+        sender.layer.cornerRadius = cornerValue
+    }
+    
+    
+    fileprivate func designSwitch(_ sender: UISwitch, onColor: UIColor, thumbColor: UIColor){
+        sender.onTintColor = .systemPink //켰을 때 컬러
+        sender.thumbTintColor = .lightGray //동그라미 컬러
+    }
+    
+    fileprivate func designLabel(_ sender: UILabel, labelText: String, textColor: UIColor, fontAttribute: UIFont){
+        sender.text = labelText
+        sender.textColor = textColor
+        sender.font = fontAttribute
+    }
 }
